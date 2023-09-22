4f2b1597076edba5a2e400bb3e33b2f0ee39d19e
SHUBHAM Kumar <shubham@SHUBHAMs-MacBook-Air.local>
2023-09-22 10:49:27 +0530

    mongo err11

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     await mongoose.set('strictQuery', true);
     await mongoose.connect(process.env.MONGO_URI,{
       useNewUrlParser: true,
+      useUnifiedTopology: true
      }).then(() => {
       console.log(clc.cyanBright.bold('connected to db'));
        }).catch((err) => {
